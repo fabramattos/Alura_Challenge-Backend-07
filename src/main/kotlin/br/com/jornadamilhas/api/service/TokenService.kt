db905f56aa38e86636f09a8d37bd0fda5a1dea5b
Felipe Mattos <fabramattos@gmail.com>
2023-08-18 16:14:01 -0300

    - mudado variavel para o TokenService. Colocado em variavel local

--- src/main/kotlin/br/com/jornadamilhas/api/service/TokenService.kt
+++ src/main/kotlin/br/com/jornadamilhas/api/service/TokenService.kt
@@ -5,24 +5,27 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.exceptions.JWTCreationException
 import com.auth0.jwt.exceptions.JWTVerificationException
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.time.Duration
 import java.time.Instant
 
 
 @Service
-class TokenService {
-
-    private final val ALGORITMO = Algorithm.HMAC512("secret")
-    private final val ISSUER = "API JornadaMilhas"
-    private final val DIAS_EXPIRACAO = diasValidade(3)
+class TokenService(
+    @Value("\${jwt.secret}")
+    private val JWT_SECRET: String,
+    private val ALGORITMO: Algorithm = Algorithm.HMAC512(JWT_SECRET),
+    private val ISSUER: String = "API JornadaMilhas",
+    private val DIAS_EXPIRACAO: Long = 3
+) {
     fun geraToken(usuario: Usuario) =
         try {
             JWT
                 .create()
                 .withIssuer(ISSUER)
                 .withSubject(usuario.login)
-                .withExpiresAt(DIAS_EXPIRACAO)
+                .withExpiresAt(diasValidade(DIAS_EXPIRACAO))
                 .sign(ALGORITMO)
         } catch (e: JWTCreationException) {
             throw RuntimeException("Erro ao gerar jwt")
