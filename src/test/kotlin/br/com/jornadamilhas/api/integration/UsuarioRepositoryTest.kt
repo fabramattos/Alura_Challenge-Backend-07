ac7757010a705d6a000bcb5d86358ad4d1fdfc7d
Felipe Mattos <fabramattos@gmail.com>
2023-08-23 17:15:40 -0300

    - corrigido problemas para testes em conteiners

--- src/test/kotlin/br/com/jornadamilhas/api/integration/UsuarioRepositoryTest.kt
+++ src/test/kotlin/br/com/jornadamilhas/api/integration/UsuarioRepositoryTest.kt
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@DataJpaTest
+@Testcontainers
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class UsuarioRepositoryTest(@Autowired private val repository: UsuarioRepository) {
+class UsuarioRepositoryTest(@Autowired private val repository: UsuarioRepository)
+    : DatabaseContainerConfiguration(){
 
     private val usuario = UsuarioTest.build()
 
