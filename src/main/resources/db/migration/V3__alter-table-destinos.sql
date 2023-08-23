febe5928845882139a605f949b23cdb2e91b2c59
Felipe Mattos <fabramattos@gmail.com>
2023-08-23 15:40:40 -0300

    - criada configuração docker-compose.yml para facilitar subir servidorr mysql.  - Alterada migration que cria as colunas na tabela de destinos

--- src/main/resources/db/migration/V3__alter-table-destinos.sql
+++ src/main/resources/db/migration/V3__alter-table-destinos.sql
@@ -8,7 +8,7 @@ ALTER TABLE destino
 ADD COLUMN meta varchar(160) not null;
 
 ALTER TABLE destino
-ADD COLUMN descricao varchar(1024);
+ADD COLUMN descricao text;
 
 UPDATE destino
 SET foto1Url = foto;
